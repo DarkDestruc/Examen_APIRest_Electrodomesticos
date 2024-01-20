1a0449dc3a0a2ec4f712209e8947f1cb47b2b31a
Moises Chavez <gchavez19956@gmail.com>
2024-01-19 20:52:42 -0500

    Add project files.

---
+++ Examen_APIRest_Electrodomesticos/Controllers/ElectrodomesticosControllers.cs
@@ -0,0 +1,51 @@
+﻿using Examen_APIRest_Electrodomesticos.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Reflection;
+
+
+namespace Examen_APIRest_Electrodomesticos.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    
+    public class ElectrodomesticosControllers: ControllerBase
+    {
+        private static List<AlmacenElectrodomesticos> _Electrodomesticos = new List<AlmacenElectrodomesticos>
+        {
+            new AlmacenElectrodomesticos
+            {
+                IdProduct = 1,
+                NameProductElectrod = "Cocinas",
+            },
+
+            new AlmacenElectrodomesticos
+            {
+                IdProduct = 2,
+                NameProductElectrod = "Neveras",
+            },
+
+            new AlmacenElectrodomesticos
+            {
+                IdProduct = 3,
+                NameProductElectrod = "Lavadoras",
+            },
+
+            new AlmacenElectrodomesticos
+            {
+                IdProduct = 4,
+                NameProductElectrod = "Televisores",
+            },
+
+
+        };
+        // Consultar todos los Electrodomestics
+        [HttpGet]
+        public IEnumerable<AlmacenElectrodomesticos> GetAllElectrodomesticos()
+        { 
+        return _Electrodomesticos;
+        }
+
+     }
+}
---
+++ Examen_APIRest_Electrodomesticos/Models/AlmacenElectrodomesticos.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen_APIRest_Electrodomesticos.Models
+{
+    public class AlmacenElectrodomesticos
+    {
+        #region Properties
+        public int IdProduct { get; set; }
+        public string NameProductElectrod { get; set; }
+        #endregion
+    }
+}
